661c1dbfc2e122aa063ac736fd9f5faafeeab471
Bran47 <93209532+Bran47@users.noreply.github.com>
2021-11-22 16:41:49 +0300

    Add files via upload

---
+++ degree.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main() 
+{
+	
+	float fahrenheit, celsius, formula;
+	cout<<"enter fahrenheit";
+	cin>> fahrenheit;
+	cout<<"enter in celsius";
+	cin>>celsius;
+	
+	formula= celsius=5.0/9.0*(fahrenheit=-32.0);
+	cout<<"calculate celsius to fahrenheit";
+	cin>>formula;
+	return 0;
+	
+}
